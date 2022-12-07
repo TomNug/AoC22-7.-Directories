5eabc2dc2498b6be2a21bb4494d25b21d7d77c94
T Nugent <t.nugent@hotmail.co.uk>
2022-12-07 19:20:00 +0000

    Refactoring code. Cleaning house.

--- main.cs
+++ main.cs
@@ -1,7 +1,47 @@
 using System;
 using System.Collections.Generic;
 
+class Directory
+{
+  public List<Node> nodes; // Modifiable
+  public List<NodeChildrenPair> parentChildren; // Modifiable
 
+  public Directory(List<NodeChildrenPair> pc) {
+    //nodes = n;
+    parentChildren = pc;
+  }
+  // Calculate the subtree associated with the parent
+  public int CalculateSizeRecursive(NodeChildrenPair node) {
+    //if (node.getChildren().Count == 0)
+        return 0;}
+  
+  public int CalculateSize() {
+    int totalSizeUnder100k = 0;
+    foreach(NodeChildrenPair node in parentChildren) {
+      //Console.WriteLine("{0}: has {1} children", node.getNode(), node.getChildren().Count);
+      // Calculate the size of each parent
+      int size =  CalculateSizeRecursive(node.getNode());
+      //Console.WriteLine("Parent: {0}", parent);
+      totalSizeUnder100k += size;
+    }
+    Console.WriteLine("Total under 100k: {0}", totalSizeUnder100k);
+    return 0;
+  }
+
+  
+}
+
+class TreeNode {
+  private string name;
+  private int size;
+  private List<TreeNode> childList;
+
+  public TreeNode(string n, int s, List<TreeNode> c) {
+    name = n;
+    size = s;
+    childList = c;
+  }
+}
 class Node
 {
   private string node;
@@ -21,84 +61,31 @@ class Node
 // Need to create a parent children class
 class NodeChildrenPair
 {
-  private string node;
+  private Node node;
   private List<Node> children;
 
-  public NodeChildrenPair(string n, List<Node> l) {
+  public NodeChildrenPair(Node n, List<Node> l) {
     node = n;
     children = l;
   }
-  public string getNode() {return node;}
+  public string getNode() {return node.getNode();}
   public List<Node> getChildren() {return children;}
 }
 
 class Program {
-  // Calculate the subtree associated with the parent
-  public static int CalculateSize(List<Node> nodes, IDictionary<string, int> nodeSizes, string parent) {
-    int dirSize = 0;
-    foreach(Node node in nodes) {
-      if (node.getParent() == parent) {
-        int size = node.getSize();
-        if (size == -1) {
-          //dirSize += CalculateSize(nodes, node.getNode()); 
-        } else {
-          dirSize += size;
-        }
-      }
-    }
-    if (dirSize <= 100000) {
-      return dirSize;
-    } else {
-      return 0;
-    }
-  }
 
-  // Calculate the entire directory
-  public static void CalculateAllSizes(List<Node> nodes) {
 
-    
-    List<NodeChildrenPair> parentChildren = calculateParentChildLists(nodes);
-    IDictionary<string, int> nodeSizes = new Dictionary<string, int>();
-    foreach(NodeChildrenPair node in parentChildren) {
-      nodeSizes.Add(node.getNode(), node.getChildren().Count);
-    }
-
-    foreach(KeyValuePair<string, int> pair in nodeSizes)
-    {
-      Console.WriteLine("{0}:{1}", pair.Key, pair.Value);
-    }
-
-    //foreach(NodeChildrenPair node in parentChildren) {
-    //  if (size == -1) {
-    //      nodeSizes[node.getNode()] = CalculateSize(nodes, node.getNode()); 
-    //    }
-    //  }
-    //}
-  
-
-    //
-    //int totalSizeUnder100k = 0;
-    //foreach(NodeChildrenPair node in parentChildren) {
-    //  Console.WriteLine("{0}: has {1} children", node.getNode(), node.getChildren().Count);
-      // Calculate the size of each parent
-    //  int size = CalculateSize(nodes, node.getNode());
-      //Console.WriteLine("Parent: {0}", parent);
-    //  totalSizeUnder100k += size;
-    //}
-    //Console.WriteLine("Total under 100k: {0}", totalSizeUnder100k);
-  }
-
-  public static List<NodeChildrenPair> calculateParentChildLists(List<Node> nodes) {
+  /*public static List<NodeChildrenPair> calculateParentChildLists() {
     // Find all unique parents
     HashSet<string> uniqueParents = new HashSet<string>();
-    foreach (Node node in nodes) {
+    foreach (Node node in NODES) {
       uniqueParents.Add(node.getParent());
     }
     List<NodeChildrenPair> parentAndChildrenUnsorted = new List<NodeChildrenPair>();
     int biggestNumChildren = 0;
     foreach(string parent in uniqueParents) {
       List<Node> children = new List<Node>();
-      foreach(Node node in nodes) {
+      foreach(Node node in NODES) {
         if (node.getParent() == parent){
           children.Add(node);
         }
@@ -120,10 +107,53 @@ class Program {
       }
     }
     return parentAndChildrenSorted;
+  }*/
+
+
+
+  // Calculate the size of a directory defined by a list of nodes. 
+  public static void CalculateAllSizes(List<NodeChildrenPair> parentsAndChildren) {
+
+    // Create a directory
+    Directory dir = new Directory(parentsAndChildren);
+    //int under100k = dir.CalculateSize();
   }
 
 
+  public static void DisplayParentChildPairs(List<NodeChildrenPair> parentChildPairs) {
+    foreach (NodeChildrenPair pair in parentChildPairs) {
+      Console.WriteLine("Parent: {0}", pair.getNode().getNode());
+      foreach (Node node in pair.getChildren()) {
+        Console.WriteLine("\t{0}", node.getNode());
+      }
+    }
+  }
 
+  
+  // Calculates a list of NodeChildrenPair from a list of Nodes
+  public static List<NodeChildrenPair> calculateParentChildLists(List<Node> nodes) {
+    // Find all unique parents
+    HashSet<string> uniqueParents = new HashSet<string>();
+    foreach (Node node in nodes) {
+      uniqueParents.Add(node.getParent());
+    }
+    
+    List<NodeChildrenPair> parentAndChildren = new List<NodeChildrenPair>();
+    // For each parent
+    foreach(string parent in uniqueParents) {
+      List<Node> children = new List<Node>();
+      foreach(Node node in nodes) {
+        if (node.getParent() == parent){
+          children.Add(node);
+        }
+      }
+      NodeChildrenPair newPair = new NodeChildrenPair(parent, children);
+      parentAndChildren.Add(newPair);
+    }
+    return parentAndChildren;
+  }
+
+  
   
   public static void Main (string[] args) {
     string[] lines = System.IO.File.ReadAllLines(@"input.txt");
@@ -163,6 +193,12 @@ class Program {
         nodes.Add(newNode);
       }
     } // end foreach
-    CalculateAllSizes(nodes);
+
+    // Parsed all commands. Have a list of Nodes
+    
+    List<NodeChildrenPair> parentChildPairs = calculateParentChildLists(nodes);
+
+    DisplayParentChildPairs(parentChildPairs);
+    //CalculateAllSizes(parentChildPairs);
   }
 }
